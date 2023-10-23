8a0af76d4144e85616276d503fa6ebe582a8865b
Henrique Beier <ahoy@harbortype.com>
2023-10-23 17:48:58 -0300

    --- INITIAL VERSION OF MY BLOG ---

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "Henrique Beier",
+	url: "https://henriquebeier.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "My own personal web.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Henrique Beier",
+		email: "blog@henriquebeier.com",
+		url: "https://henriquebeier.com/about-me/"
 	}
 }
--- content/about/index.md
+++ content/about/index.md
@@ -2,8 +2,8 @@
 layout: layouts/base.njk
 eleventyNavigation:
   key: About Me
-  order: 3
+  order: 4
 ---
 # About Me
 
-I am a person that writes stuff.
+I am a type designer and font engineer based in Porto Alegre, Brazil. I work daily at [Fabio Haag Type](https://fabiohaagtype.com/en/) and [Harbor Type](https://harbortype.com). Every now and then I also lend my font engineer   hand to other type foundries.
