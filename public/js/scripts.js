7a073d4ab016b01eab2146b938958a83e59de787
Henrique Beier <ahoy@harbortype.com>
2023-11-13 21:29:00 -0300

    add scripts.js

---
+++ public/js/scripts.js
@@ -0,0 +1,10 @@
+// Attach a click handler to the search link
+var searchLink = document.getElementById("search-link");
+searchLink.addEventListener("click", function(event) {
+
+  // Don't navigate away
+  event.preventDefault();
+
+  // Search here instead...
+
+}, false);
